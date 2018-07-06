e0ff607224f3363209c3a65c0a5b8e4a369435f7
LAPTOP-URF7MCEU\TheQueen <kalaxander@yahoo.com>
2018-07-05 21:35:24 -0400

    Splash, Start, Options, Level 01
    very basic, but start screen has fade in!

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+   public float autoLoadNextLevelDelay;
+
+   void Start()
+    {
+        if (autoLoadNextLevelDelay > 0) // probably only used for the splash screen
+            Invoke("LoadNextLevel", autoLoadNextLevelDelay);
+    }
+
+   public void LoadNextLevel()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int nextScene = scene.buildIndex + 1;
+        SceneManager.LoadScene(nextScene);
+    }
+
+    public void LoadLevel(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+
+  
+}
---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip[] levelMusic;
+
+    AudioSource audioSource;
+
+    // this causes the music manager to persist accross levels
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+        audioSource = GetComponent<AudioSource>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    //the following three functions replace the depracated private void OnLevelWasLoaded(int level)
+    void OnEnable()
+    {
+        //Tell our 'OnLevelLoaded' function to start listening for a scene change as soon as this script is enabled.
+        SceneManager.sceneLoaded += OnLevelLoaded;
+    }
+
+    void OnDisable()
+    {
+        //Tell our 'OnLevelLoaded' function to stop listening for a scene change as soon as this script is disabled. 
+        // Remember to always have an unsubscription for every delegate you subscribe to!
+        SceneManager.sceneLoaded -= OnLevelLoaded;
+    }
+    
+    private void OnLevelLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (levelMusic[scene.buildIndex])
+        {
+            audioSource.clip = levelMusic[scene.buildIndex];
+            audioSource.loop = true;
+            audioSource.Play();
+        }
+    }
+}
